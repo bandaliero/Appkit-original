04077a2e027a4777cb5f383da20893e66b144926
bandaliero <bandaliero@gmail.com>
2021-09-04 13:31:43 +0300

    nav-header part

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
-console.log("hello world")
+// (function () {
+//     let menu = document.getElementsByClassName("nav-burger");
+//     let links = document.getElementsByClassName("nav-links");
+//     let linkStyle = getComputedStyle(links);
+//
+//     menu.addEventListener("click", function () {
+//         if(linkStyle.display === 'none'){
+//             links.classList.add('active')
+//         } else {
+//             links.classList.remove('active')
+//         }
+//     })
+// })()
+let menu = document.getElementById('burger');
+let links = document.getElementById('links');
+let linkStyle = getComputedStyle(links);
+
+menu.addEventListener("click", function () {
+    if(linkStyle.display === 'none'){
+        links.classList.add('active')
+    } else {
+        links.classList.remove('active')
+    }
+})
