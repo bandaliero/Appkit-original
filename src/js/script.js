592dfb238306b16d4cb2f6ba62f92576ea557345
bandaliero <bandaliero@gmail.com>
2021-09-05 17:19:05 +0300

    header complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,3 @@
-// (function () {
-//     let menu = document.getElementsByClassName("nav-burger");
-//     let links = document.getElementsByClassName("nav-links");
-//     let linkStyle = getComputedStyle(links);
-//
-//     menu.addEventListener("click", function () {
-//         if(linkStyle.display === 'none'){
-//             links.classList.add('active')
-//         } else {
-//             links.classList.remove('active')
-//         }
-//     })
-// })()
 let menu = document.getElementById('burger');
 let links = document.getElementById('links');
 let linkStyle = getComputedStyle(links);
